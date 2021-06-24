1f7e734ae3d674032c9dd42c37fd8389a49b604b
David <may9noy@gmail.com>
2021-06-25 00:12:04 +0900

    2021-06-21_01

--- src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -19,22 +19,20 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
     @Override
     public GatewayFilter apply(Config config) {
         // Custom pre Filter
-        return (exchange, chain) -> {
+        return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
             log.info("Global Filter baseMessage: {}", config.getBaseMessage());
-
             if (config.isPreLogger()) {
                 log.info("Global Filter Start: request id -> {}", request.getId());
             }
-
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
                     log.info("Global POST filter End: response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        });
     }
 
     @Data
